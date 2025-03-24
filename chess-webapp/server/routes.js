dabb791a5d59714355e4021dd85b8eb22d95907b
Rayyan PC <Rayyan9000@outlook.com>
2025-03-24 01:20:06 +0000

    Changed routes back to normal

--- chess-webapp/server/routes.js
+++ chess-webapp/server/routes.js
@@ -4,15 +4,8 @@ module.exports = (app) => {
     const clientPath = path.join(__dirname, '../client');
 
     // Default Route (Main Menu)
-    /*
     app.get('/', (req, res) => {
         res.sendFile(path.join(clientPath, 'menu', 'mainmenu', 'mainmenu.html'));
-    });
-    */
-    
-    // Route for game
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(clientPath, 'game', 'index.html')); // Serve the game.html file
     });
 
     // Route for singleplayer
